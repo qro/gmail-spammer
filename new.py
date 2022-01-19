da3ea55295930acd522259bd6267296a5b0df08b
Laura Clark <afqro@outlook.com>
2022-01-19 12:55:21 +0000

    smtp login

--- new.py
+++ new.py
@@ -16,3 +16,14 @@ class SMTP():
         self.victim = victim
         self.message = message
         self.number = number
+    
+    def verify(self):
+        server1 = smtplib.SMTP('smtp.gmail.com', 587) # change this to any smtp server you want
+        server1.ehlo()
+        server1.starttls()
+
+        try:
+            server1.login(self.email, self.password)
+        except smtplib.SMTPAuthenticationError:
+            print('\n [!] The email or password is wrong\n [!] Make sure lesssecureapps is turned on')
+            input()
