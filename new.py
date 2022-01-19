fe157f6f0402a15b4d2bdea155a24c74f934f2ba
Laura Clark <afqro@outlook.com>
2022-01-19 12:52:33 +0000

    object orientation

--- new.py
+++ new.py
@@ -4,3 +4,15 @@ with open('config.json') as f:
     config = json.load(f)
 email = config.get('email')
 password = config.get('password')
+message = config.get('message')
+number = config.get('amount')
+
+victim = input(' [?] Victim: ')
+
+class SMTP():
+    def __init__(self):
+        self.email = email
+        self.password = password
+        self.victim = victim
+        self.message = message
+        self.number = number
